71b9672a4f0e9ff83bff36a07acf0e746331b159
Hieu Vijjo <hieumcao@gmail.com>
2019-12-08 06:17:55 +0700

    Use `random.sample` to pick randomly from lists

--- madness_generator.py
+++ madness_generator.py
@@ -13,16 +13,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c',
                     '--concrete',
                     default=CONCRETE_TXT,
-                    nargs='?',
                     help='path to text of concrete')
 parser.add_argument('-a',
                     '--abstract',
                     default=ABSTRACT_TXT,
-                    nargs='?',
                     help='path to text of abstract')
 parser.add_argument('-n',
                     '--number',
                     default='1',
+                    const='1',
                     type=int,
                     nargs='?',
                     help='path to text of abstract')
@@ -30,19 +29,21 @@ parser.add_argument('-n',
 args = parser.parse_args()
 
 with open(args.concrete) as file:
-    concrete = file.read().splitlines()
+    concretes = file.read().splitlines()
 
 with open(args.abstract) as file:
-    abstract = file.read().splitlines()
+    abstracts = file.read().splitlines()
 
-if len(concrete) > 0 and len(abstract) > 0:
+# __import__('pprint').pprint(vars(args))
+
+if len(concretes) > 0 and len(abstracts) > 0:
     book = {}
     max_length = 0
-    for number in range(1, args.number + 1):
-        random_concrete = random.randint(0, len(concrete) - 1)
-        random_abstract = random.randint(0, len(abstract) - 1)
-        book[concrete[random_concrete]] = abstract[random_abstract]
-        if len(concrete[random_concrete]) > max_length:
-            max_length = len(concrete[random_concrete])
-    for key, value in book.items():
-        print(f'{key.ljust(max_length)}   {value}')
+    random_concretes = random.sample(concretes, args.number)
+    for item in random_concretes:
+        if len(item) > max_length:
+            max_length = len(item)
+    random_abstracts = random.sample(abstracts, args.number)
+    for index in range(0, args.number):
+        print(f'{random_concretes[index].ljust(max_length+3)}'
+              f'{random_abstracts[index]}')
